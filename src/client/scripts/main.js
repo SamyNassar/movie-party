90f6a47a87a9e125ef95fb7f0b65f4fca8729f5d
Samy Nassar <samy0ayman@gmail.com>
2021-05-17 00:59:12 +0200

    Implement connect WS client

--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -1,11 +1,16 @@
-const inputElement = document.getElementById("input");
+
+const inputMedia = document.getElementById("media-path");
 const mediaPlayer = document.getElementById("myVideo");
+const createButton = document.getElementById("create-party");
+const joinButton = document.getElementById("join-party");
+const partyCode = document.getElementById("party-code");
 
 
 
 
-inputElement.addEventListener("change", handleMedia, false);
 
+
+inputMedia.addEventListener("change", handleMedia, false);
 function handleMedia() {
   const media = this.files[0];
   const mediaURL = window.URL.createObjectURL(media);
@@ -13,4 +18,43 @@ function handleMedia() {
 }
 
 
+// Create Party listener.
+createButton.addEventListener("click", () => {
+  console.log("Create Party!!");
 
+  const ws = new WebSocket('ws://localhost:9091');
+
+  // Connection opened
+  ws.addEventListener('open', function (event) {
+    ws.send('Hello Server, Please create my party!');
+  });
+
+  ws.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+  });
+
+})
+
+// Join Party listener.
+joinButton.addEventListener("click", () => {
+  console.log("Join Party!!");
+  console.log(partyCode.value);
+
+})
+
+
+
+
+// // Connection opened
+// ws.addEventListener('open', function (event) {
+//     ws.send('Hello Server!');
+// });
+
+// // Listen for messages
+// ws.addEventListener('message', function (event) {
+//     console.log('Message from server ', event.data);
+// });
+
+// ws.onmessage = message => {
+//     console.log('Message from server ', message);
+// }

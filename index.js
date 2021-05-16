938d5eb9a3da9508e7623b6c951e98b8f0fde7b4
Samy Nassar <samy0ayman@gmail.com>
2021-05-17 00:58:18 +0200

    Implement connect method WS server

--- index.js
+++ index.js
@@ -1,18 +1,88 @@
 const express = require('express');
 const path = require('path');
+const WebSocketServer = require('websocket').server;
+const http = require('http');
 
 
+const PORT = 8080;
+
+// Operation Constant.
+const CONNECT = 0;
+const CREATE_PARTY = 1;
+const JOIN_PARTY = 2;
+
+
+// Initialize Express app.
 const app = express();
-const port = 8080;
-
 app.use(express.static(path.join(__dirname, 'src/client')))
+app.get('/', (req, res) => res.sendFile('index.html'))
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
 
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html');
+
+
+const httpserver = http.createServer();
+httpserver.listen(9091, () => console.log(`http listening at http://localhost:9091`))
+
+
+const wsServer = new WebSocketServer({
+    httpServer: httpserver
+});
+
+
+
+
+wsServer.on('request', request => {
+    const connection = request.accept(null, request.origin);
+    console.log("Request Accepted !!")
+    // console.log(connection)
+
+    connection.on("open", () => console.log("OPEND!!"));
+    connection.on("close", () => console.log("CONNECTION CLOSED!!"));
     
-})
+    connection.on("message", message => {
+        // Message From Client.
+        const msg = JSON.parse(message.utf8Data)
+        console.log(msg);
+        
+        switch(msg.method){
+            case CREATE_PARTY:
+                // TODO : createParty() method.
+                // createParty();
+                console.log("CREATING A PARTY !!");
+                connection.send(JSON.stringify({status: "CREATE"}));
+                break;
+            case JOIN_PARTY:
+                // TODO : joinParty() method.
+                // joinParty();
+                console.log("JOINING A PARTY !!");
+                connection.send(JSON.stringify({status: "JOIN"}));
+                break;
+            default:
+                console.log(msg);
+        }
+
+
+    });
+
+
+    const res = {
+        method: CONNECT,
+        clientId : 1512315, // TODO <guid>
+    }
+
+    connection.send(JSON.stringify(res));
+});
+
+// wsServer.on('connect', () => {
+//     console.log("CONNECTED!!");
+// })
+// wsServer.on('close', () => {
+//     console.log("WS CLOSED!!");
+// })
+
+
   
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-})
+
+
+
